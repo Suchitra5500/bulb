9ed60d4db101687f388c10dcfeb4b261dad8956a
Suchitra5500 <suchitrashekhargol5500@gmail.com>
2021-12-16 22:27:27 +0530

    bulb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {useState} from 'react'
+import './style.css'
 function App() {
+
+  const [blub,setblub] = useState(false)
+  const makeBlubOn = () => {
+    setblub(true)
+    
+
+  }
+  const makeBlubOff = () => {
+    setblub(false)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <center>
+      <h1 className='h1'>The Bulb Show</h1>
+        {
+          blub===true? <img width='300px' height='300px' src='https://previews.123rf.com/images/murika/murika1511/murika151100069/48123160-bright-glowing-incandescent-light-bulb-on-a-white-background.jpg' alt='not found'/> :<img width='300px' height='300px' src='https://previews.123rf.com/images/ericmilos/ericmilos0912/ericmilos091200136/6109526-3d-render-of-light-bulb-on-white.jpg' alt='not found' />
+    
+        }
+        <br></br><br></br>
+            <button className='On' onClick={makeBlubOn}>Blub ON</button><br></br>
+            <button className='Off' onClick={makeBlubOff}>Blub OFF</button><br></br>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
